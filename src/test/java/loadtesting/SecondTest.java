d9b2cd1c401ba0f62626c109bdf34f68fe024826
Javier Perez Martin <permarja@gmail.com>
2017-07-31 12:21:59 +0200

    adsf!

---
+++ src/test/java/loadtesting/SecondTest.java
@@ -0,0 +1,47 @@
+package loadtesting;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class GoogleHomePageTest {
+
+    private WebDriver driver;
+    String appURL = "http://google.com";
+
+    @BeforeClass
+    public void testSetUp() throws MalformedURLException {
+
+        //driver = new FirefoxDriver();
+
+        DesiredCapabilities capability = DesiredCapabilities.firefox();
+        // Say you want a specific node to thread your request, just specify the node name (it must be running a selenium configuration though)
+        capability.setBrowserName("firefox");
+        
+        driver = new RemoteWebDriver(new URL("http://selenium-hub:4444/wd/hub"), capability);
+        
+        System.out.println("secondTest");
+    }
+
+    @Test
+    public void verifyGooglePageTittle() {
+        //driver.navigate().to(appURL);
+        //String getTitle = driver.getTitle();
+        //Assert.assertEquals(getTitle, "Google");
+
+        driver.get("http://www.marca.com");
+        Assert.assertEquals(driver.getTitle(), "Marca");
+    }
+
+    @AfterClass
+    public void tearDown() {
+        driver.quit();
+    }
+
+}
