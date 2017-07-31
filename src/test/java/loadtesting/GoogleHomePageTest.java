f9d300339ae7ef530c9263caa930593032c7ed68
Javier Perez Martin <permarja@gmail.com>
2017-07-31 14:03:27 +0200

    sdf

--- src/test/java/loadtesting/GoogleHomePageTest.java
+++ src/test/java/loadtesting/GoogleHomePageTest.java
@@ -37,7 +37,7 @@ public class GoogleHomePageTest {
 
         driver.get("http://www.google.es");
         Assert.assertEquals(driver.getTitle(), "Google");
-		driver.get("https://es.yahoo.com");
+		driver.get("pepippt");
         Assert.assertEquals(driver.getTitle(), "Yahoo");
     }
 
